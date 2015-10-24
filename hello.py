f1da7261b3a3541384f344a4b8c7e61645e0de40
CorySpitzer <coryspitzer@gmail.com>
2015-10-24 16:59:44 -0700

    Greet people by name

--- hello.py
+++ hello.py
@@ -2,9 +2,15 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "<h>Hello, <b>World!</b></h>"
+def hello_world():
+    return "<h>Hello, Oh <b>World!</b></h>"
+
+@app.route('/<name>')
+def hello(name):
+    return "Hello " + name.capitalize()
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    app.run(debug = True)
+    # or
+    # app.debug = True
+    # app.run()
